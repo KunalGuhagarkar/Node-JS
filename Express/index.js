09889693296736ab92a4512b22cdda90b54d7cff
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-03-17 13:06:29 +0530

    Express commited

---
+++ Express/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>");
+});
+
+app.post("/register", (req, res) => {
+    res.sendStatus(201);
+});
+
+app.put("/user/kunal", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.patch("/user/kunal", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.delete("/user/kunal", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}`);
+});
