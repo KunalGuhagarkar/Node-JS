ddd950f257017022d1e5e1103fa6a4b970307730
KunalGuhagarkar <kunal.workspace13@gmail.com>
2026-03-18 12:02:57 +0530

    Commited EJS Weekend or Weekday Project

---
+++ Express/EJS/index.js
@@ -0,0 +1,24 @@
+// Functionality of getDay()
+
+// imports
+import express from 'express';
+
+let day = new Date().getDay();
+
+function weekendOrWeekday(day) {
+    if (day !== 7 || day !== 0) {
+        return 'Hey its a Weekday :(';
+    } else {
+        return 'Hey its a Weekend :)';
+    }
+}
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render("index.ejs", {day : weekendOrWeekday(day)});
+});
+
+app.listen(3000, () => {
+    console.log('Server Listening at Port 3000');
+});
